a3841e55147ebaa112958d75c6a3603b0eed1a7f
sailokeshbommi2004-dotcom <sailokeshbommi2004@gmail.com>
2026-04-13 15:44:48 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,11 +1,10 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__)   # ✅ THIS MUST BE EXACT NAME
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-# Important for deployment
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
