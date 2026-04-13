fc6eae1c4413e6ec946b5b2913a5038ad965fd2e
sailokeshbommi2004-dotcom <sailokeshbommi2004@gmail.com>
2026-04-13 15:26:55 +0530

    Update Flask app run configuration for deployment
    Change app to run on host 0.0.0.0 and port 10000 for deployment.

--- app/app.py
+++ app/app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -7,5 +6,6 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+# Important for deployment
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
